71c1a4bbd082e2847f6e84d025aaee0e6a9502fb
S Bhagya Nandini <bhagyanandinisuravarapu@gmail.com>
2026-06-22 11:43:23 +0530

    Finalize: Factory Method updates and READMEs

--- DotNet FSE/DeepSkilling/Design patterns and principles in Engineering concepts/Implementing the Factory Method Pattern/WordDocument.java
+++
@@ -1,8 +0,0 @@
-public class WordDocument implements Document {
-    private final String content;
-    public WordDocument() { this.content = "Word document content"; }
-    @Override
-    public String getType() { return "Word"; }
-    @Override
-    public void open() { System.out.println("Opening Word document: " + content); }
-}
